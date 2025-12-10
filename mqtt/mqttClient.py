dad4430c03eb2bb98558065c01c6d6896bdfc389
sonseokmin <son1245960@gmail.com>
2025-12-11 00:55:00 +0900

    FIX MQTTconfig.py

--- mqtt/mqttClient.py
+++ mqtt/mqttClient.py
@@ -1,17 +1,17 @@
 import ssl
 from fastapi_mqtt import FastMQTT, MQTTConfig
 
-from config import CA_CERT_PATH
+from config import CA_CERT_PATH, HOST, PASSWORD, PORT, USERNAME
 
 ctx = ssl.create_default_context(cafile=CA_CERT_PATH)
 ctx.check_hostname = False        
 ctx.verify_mode = ssl.CERT_REQUIRED
 
 mqttConfig = MQTTConfig(
-    host="127.0.0.1",
-    port=8883,
-    username="sechang",
-    password="1234",
+    host=HOST,
+    port=PORT,
+    username=USERNAME,
+    password=PASSWORD,
     client_id="RPI_GATEWAY_01",
     ssl=ctx,
     keepalive=120
